24fdd7f99d46692f06e4a5a98c998db27372ae85
Azis Hapidin <azishapidin@gmail.com>
2018-09-03 22:12:03 +0700

    initialize

---
+++ woo-whatsapp.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: WooWhatsApp
+ * Plugin URI: https://github.com/azishapidin/woo-whatsapp
+ * Description: 
+ * Version: 0.9.1
+ * Author: Azis Hapidin
+ * Author URI: https://azishapidin.com/
+ * License: MIT License
+ */
+?>
