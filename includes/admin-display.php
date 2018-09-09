6be08c10b4df379416ae7e4233ef7093ef88edbb
Azis Hapidin <azishapidin@gmail.com>
2018-09-09 07:43:08 +0700

    update condition

--- includes/admin-display.php
+++ includes/admin-display.php
@@ -17,21 +17,21 @@ if (count($_POST) > 0) {
      
     }
     if (isset($_POST['woo_wa_phone_number']) && is_null($errorMessage)) {
-        if (!get_option('woo_wa_phone_number') || strlen(get_option('woo_wa_phone_number')) == 0) {
+        if (!get_option('woo_wa_phone_number')) {
             add_option( 'woo_wa_phone_number', $_POST['woo_wa_phone_number'] );
         } else {
             update_option( 'woo_wa_phone_number', $_POST['woo_wa_phone_number'] );
         } 
     }
     if (isset($_POST['woo_wa_content']) && is_null($errorMessage)) {
-        if (!get_option('woo_wa_content') || strlen(get_option('woo_wa_content')) == 0) {
+        if (!get_option('woo_wa_content')) {
             add_option( 'woo_wa_content', $_POST['woo_wa_content'] );
         } else {
             update_option( 'woo_wa_content', $_POST['woo_wa_content'] );
         }
     }
     if (isset($_POST['woo_wa_button']) && is_null($errorMessage)) {
-        if (!get_option('woo_wa_button') || strlen(get_option('woo_wa_button')) == 0) {
+        if (!get_option('woo_wa_button')) {
             add_option( 'woo_wa_button', $_POST['woo_wa_button'] );
         } else {
             update_option( 'woo_wa_button', $_POST['woo_wa_button'] );
