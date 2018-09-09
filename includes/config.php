42da3081dca9aaad347ef6d78892602b94f25da5
Azis Hapidin <azishapidin@gmail.com>
2018-09-09 14:25:59 +0700

    update code

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- * 
  * Define configuration for this plugin.
  */
 
@@ -9,3 +8,24 @@ $wooWhatsAppDefault = [
     'content'   => 'Hello, I want to buy this product {{link}}',
     'button'    => 'Order via WhatsApp',
 ];
+
+/**
+ * Add content getter function.
+ * 
+ * @param string $format Content Format.
+ * @param \WC_Data\WC_Abstract_Legacy_Product\WC_Product $product WooCommerce Product
+ */
+function getContent($format = '', $product)
+{
+    if ($format == '') {
+        $format = $wooWhatsAppDefault['content'];
+    }
+    $data = [];
+    $data['title'] = $product->get_title();
+    $data['link'] = get_permalink($product->get_id());
+    foreach ($data as $key => $value) {
+        $format = str_replace('{{' . $key . '}}', $value, $format);
+    }
+
+    return $format;
+}
--- includes/public.php
+++ includes/public.php
@@ -1,26 +1,22 @@
 <?php
+require_once __DIR__ . '/config.php'; 
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
 global $product;
-$data = [];
-$data['title'] = $product->get_title();
-$data['link'] = get_permalink($product->get_id());
+
 $phoneNumber = esc_attr( get_option('woo_wa_phone_number') );
 $content = esc_attr( get_option('woo_wa_content') );
 $button = esc_attr( get_option('woo_wa_button') );
-foreach ($data as $key => $value) {
-	$content = str_replace('{{' . $key . '}}', $value, $content);
-}
 ?>
 
 <button id="chat-wa" type="button" onclick="openWA()"><?php echo $button ?></button>
 <script>
 function openWA(){
 	var t = "<?php echo esc_attr( $phoneNumber ); ?>",
-    	a = "<?php echo esc_attr( $content ) ?>";
+    	a = "<?php echo esc_attr( getContent($content, $product) ) ?>";
 	if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) var e = "https://wa.me/" + t + "?text=" + a;
 	else e = "https://web.whatsapp.com/send?phone=" + t + "&text=" + a;
 	var n = window.open(e, "_blank");
