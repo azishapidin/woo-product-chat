6a111233b25d7c2286894c266f9bdea2fd4a17f1
Azis Hapidin <azishapidin@gmail.com>
2018-09-23 09:29:09 +0700

    update code in public

--- includes/public.php
+++ includes/public.php
@@ -15,11 +15,15 @@ $button = esc_attr($wooWhatsAppObject->getOption('woo_wa_button'));
 <button class="single_add_to_cart_button button" id="chat-wa" type="button" onclick="openWA()"><?php echo $button ?></button>
 <script>
 function openWA(){
-	var t = "<?php echo esc_attr( $phoneNumber ); ?>",
-    	a = "<?php echo esc_attr( $wooWhatsAppObject->getContent($content, $product) ) ?>";
-	if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) var e = "https://wa.me/" + t + "?text=" + a;
-	else e = "https://web.whatsapp.com/send?phone=" + t + "&text=" + a;
-	var n = window.open(e, "_blank");
+	var phoneNumber = "<?php echo esc_attr($phoneNumber); ?>",
+    	content = "<?php echo esc_attr($wooWhatsAppObject->getContent($content, $product)) ?>";
+		link = "";
+	if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+		link = "https://wa.me/" + phoneNumber + "?text=" + content;
+	} else {
+		link = "https://web.whatsapp.com/send?phone=" + phoneNumber + "&text=" + content;
+	}
+	var n = window.open(link, "_blank");
 	n ? n.focus() : alert("Please allow popups for this website")
 }
 </script>
