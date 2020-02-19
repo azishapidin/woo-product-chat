adc21f0f2b70c19bab972f24f74d7e4c8337fe0f
Azis Hapidin <azishapidin@gmail.com>
2020-02-19 11:38:33 +0700

    Update public.php

--- includes/public.php
+++ includes/public.php
@@ -14,8 +14,10 @@ if (wp_is_mobile() || $wooWhatsAppObject->getOption('woo_wa_button_show_desktop'
 	?>
 	<button class="<?php echo $wooWhatsAppObject->getOption('woo_wa_button_class', $wooWhatsAppObject->default['']) ?>" id="<?php echo $wooWhatsAppObject->getOption('woo_wa_button_id', 'woowhatsapp-button') ?>" style="<?php echo $wooWhatsAppObject->getOption('woo_wa_button_css') ?>" type="button" onclick="openWA()"><?php echo $button ?></button>
 	<script>
-		var isMobile = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i);
+		
 		function openWA(){
+			var isMobile = navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i);
+			
 			var phoneNumber = "<?php echo esc_attr($phoneNumber); ?>",
 				content = "<?php echo esc_attr($wooWhatsAppObject->getContent($content, $product)) ?>";
 				link = "";
